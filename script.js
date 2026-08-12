5585289c45136fa6ff364022a2b4b54e3e7fe22c
milenagabi710-star <milenagabi710@gmail.com>
2026-08-12 11:51:29 -0300

    configurações

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const inputUpload = document.getElementById('meu-upload')
-
-inputUpload.addEventListener('change', function(evento) {
-    const arquivo = evento.target.files[0]
-
-    if (arquivo) {
-        const nomeDaImagem = arquivo.name 
-        console.log(nomeDaimagem)
-    } else {
-        console.log("Nenhum arquivo selecionado.");
-    } 
-})
+const input
