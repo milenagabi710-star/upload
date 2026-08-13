988218acdd902aa33d989306a018d12b26dd2b4c
milenagabi710-star <milenagabi710@gmail.com>
2026-08-13 11:41:18 -0300

    configurações

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-const input
+const inputUpload = document.getElementById('avatar')
+
+inputUpload.addEventListener('change', function (evento) {
+
+    const arquivo = evento.target.files[0]
+
+    if (arquivo) {
+        const nomeDaImagem = arquivo.name
+
+        console.log(nomeDaImagem)
+    } else {
+        console.log("Nenhum arquivo selecionado.");
+    }
+})
+
+const nome = document.getElementById("nome")
+const b = document.getElementById("btn")
+b.addEventListener("click", () => {
+console.log(nome.value)
+})
